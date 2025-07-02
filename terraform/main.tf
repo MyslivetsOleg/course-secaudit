d22621cbf314b819a913635b60fc150efd23e9a9
MyslivetsOleg <myslivec.oleg@gmail.com>
2025-07-02 10:25:32 +0300

    Basic CI\CD

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+resource "opennebula_virtual_machine" "vm" { 
+  name         = "mf_secaudit_dmz_l01"
+  template_id  = 2451
+  memory       = 1024
+  cpu          = 2
+
+  nic = [
+    {
+      network_id = 10
+    }
+  ]
+}
+
+locals {
+  full_minio_endpoint = format("https://%s:%s", var.minio_endpoint, var.minio_port)
+}
+
+terraform {
+  required_providers {
+    opennebula = {
+      source  = "OpenNebula/opennebula"
+      version = ">= 1.4.1"
+    }
+  } 
+ backend "s3" {
+    bucket                     = var.minio_bucket
+    key                        = "tfstate/secaudit.tfstate"
+    region                     = "us-east-1"
+    endpoint                   = var.minio_endpoint
+    access_key                 = var.minio_access_key
+    secret_key                 = var.minio_secret_key
+    force_path_style           = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+  }
+}
+
+provider "opennebula" {
+  endpoint = var.opennebula_endpoint
+  username = var.opennebula_username
+  password = var.opennebula_password
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,45 @@
+variable "vm_count" {
+  description = "VM count"
+  type        = number
+  default     = 1
+}
+variable "opennebula_username" {
+  description = "OpenNebula username"
+  type        = string
+}
+
+variable "opennebula_password" {
+  description = "OpenNebula password"
+  type        = string
+  sensitive   = true
+}
+
+variable "opennebula_endpoint" {
+  description = "OpenNebula endpoint"
+  type        = string
+  sensitive   = true
+}
+
+variable "minio_endpoint" {
+  description = "MinIO S3 endpoint"
+  type        = string
+  sensitive   = true
+}
+
+variable "minio_bucket" {
+  description = "MinIO S3 Bucket name"
+  type        = string
+  sensitive   = true
+}
+
+variable "minio_bucket" {
+  description = "MinIO S3 Bucket name"
+  type        = string
+  sensitive   = true
+}
+
+variable "minio_port" {
+  description = "MinIO S3 port"
+  type        = string
+  sensitive   = true
+}
