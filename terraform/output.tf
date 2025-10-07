cb38046986736a6a0176eb84707034a7f504e8c7
MyslivetsOleg <myslivec.oleg@gmail.com>
2025-10-07 16:51:13 +0300

    change in tf-output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,11 @@
 output "demolab_dmz_L01_ip_lan_subnet" {
-  value = one([for nic in opennebula_virtual_machine.vm.template_nic : nic.computed_ip if can(regex("^10\\.", nic.computed_ip))])
+  value = one([for nic in opennebula_virtual_machine.demolab_dmz_L01.template_nic : nic.computed_ip if can(regex("^10\\.", nic.computed_ip))])
 }
 
 output "demolab_dmz_L01_id" {
-  value = opennebula_virtual_machine.vm.id
+  value = opennebula_virtual_machine.demolab_dmz_L01.id
 }
 
 output "demolab_dmz_L01_name" {
-  value = opennebula_virtual_machine.vm.name
+  value = opennebula_virtual_machine.demolab_dmz_L01.name
 }
